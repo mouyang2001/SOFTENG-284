c8f4c8517d554cdbee57710baacb508beff643b5
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-08-04 21:29:13 +1200

    changed proof file and updated task1 file type

--- proofs/assignment-1-3a.js
+++ proofs/assignment-1-3a.js
@@ -1,13 +1,13 @@
 console.clear();
 console.log( "\nRunning assignment-1-3a.js" );
 
-let n = 8;
+let n = 512;
 let count = 0;
 
 for (let i = 1; i <= n; i = i * 2) {
   for (let j = 0; j < n; j++) {
     if (i == j) {
-      for (let k = 1; k < n ** 3; k += n) {
+      for (let k = 1; k < n**3; k += n) {
         count++;
       }
     } else {
@@ -16,19 +16,15 @@ for (let i = 1; i <= n; i = i * 2) {
   }
 }
 
-let pastTutorialAnswer = (Math.floor(Math.log(n) / Math.log(2)) + 1) * (n ** 2 + (n - 1));
+let pastTutorialAnswer = (Math.floor(Math.log(n) / Math.log(2))+1) * (n**2 + (n - 1));
+let danielAnswer = (Math.ceil(Math.log(n)/Math.log(2))) * (n**2 + n - 1);
+let johnChenAnswer = (Math.floor((Math.log(n)/Math.log(2))) + 1) * (Math.ceil(((n**3-1)/n)) + n - 1);
 let rayAnswer = (n ** 2 - 1) * Math.ceil(Math.log(n) / Math.log(2)) + n * (Math.log(n) / Math.log(2) + 1);
 
+console.log("\nInput n = " + n);
 console.log("\nCalculated elementary operations = " + count);
 console.log("\nTutorial answer prediction = " + pastTutorialAnswer);
+console.log("Daniel Answer = " + danielAnswer);
+console.log("John Chen Answer = " + johnChenAnswer);
 console.log("Raymond answer prediction = " + rayAnswer);
-
-if (count == rayAnswer) {
-  console.log("\nRaymond is correct! 🙌 🧠")
-} else if (count === pastTutorialAnswer) {
-  console.log("\nPast Turial answer correct!😂");
-} else {
-  console.log("\nNo one correct");
-}
-
 
---
+++ task1.py
@@ -0,0 +1,29 @@
+# Author: Matthew Ouyang
+# Task: find the maximum profit from a list of stock prices
+# Commands to execute code:
+# > python3 task1.py < canvas.in > myout1.txt
+# > diff myout1.txt canvas.out1
+
+import sys
+
+# more efficient solution O(n)
+def find_max_profit(prices, length):
+  max_profit = prices[1] - prices[0]
+  min_price = prices[0]
+    
+  for i in range(1,length):
+    if (prices[i] - min_price > max_profit):
+      max_profit = prices[i] - min_price
+  
+    if (prices[i] < min_price):
+      min_price = prices[i]
+  return max_profit
+
+# driver code stdin/stdout
+for line in sys.stdin:
+  prices = [int(s) for s in line.split() if s.isdigit()]
+  length = prices.pop(0)
+  if length >= 2:
+    sys.stdout.write(str(find_max_profit(prices, length)) + '\n')
+  
+
