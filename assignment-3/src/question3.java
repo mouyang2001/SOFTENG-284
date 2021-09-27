32bd18f7d4361819c532ab3ebbb612bcd9213bf2
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-09-27 17:56:48 +1300

    add comments about question 3

---
+++ assignment-3/src/question3.java
@@ -0,0 +1,8 @@
+public class question3 {
+
+    // simple DFS problem, use hashset to detect duplicates. Hence, a cycle is found.
+    // https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+    public static void main(String[] args) {
+        System.out.println("question 3");
+    }
+}
