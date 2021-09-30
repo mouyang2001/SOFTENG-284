50cc0be4ad32677b0d4c1e709abf27bd21fe9f92
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-09-30 18:27:01 +1300

    find a cycle using dfs draft

--- assignment-3/src/question3.java
+++ assignment-3/src/question3.java
@@ -1,8 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Stack;
+
 public class question3 {
 
-    // simple DFS problem, use hashset to detect duplicates. Hence, a cycle is found.
-    // https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
     public static void main(String[] args) {
-        System.out.println("question 3");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        try {
+            while((line = reader.readLine()) != null) {
+                // Get the order of the graph.
+                int order = Integer.parseInt(line);
+
+                // Create empty adjacency list
+                ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+                for (int i = 0; i < order; i++) graph.add(new ArrayList<>());
+
+                // Populate adjacency list
+                for (int u = 0; u < order; u++) {
+                    String[] data = reader.readLine().split(" ");
+                    if (!Objects.equals(data[0], "")) {
+                        for (String value : data) {
+                            int v = Integer.parseInt(value);
+                            graph.get(u).add(v);
+                        }
+                    }
+                }
+
+                if (!graph.isEmpty()) System.out.println(hasCycle(graph));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int hasCycle(ArrayList<ArrayList<Integer>> graph) {
+        Stack<Integer> stack = new Stack<>();
+        int[] visited = new int[graph.size()];
+        int[] inStack = new int[graph.size()];
+        int order = graph.size();
+
+        // for every node starting from '0'
+        for (int i = 0; i < order; i++) {
+            if (visited[i] == 1) continue;
+
+            stack.push(i);
+            while (!stack.isEmpty()) {
+                int u = stack.peek();
+
+                if (visited[u] == 0) {
+                    visited[u] = 1;
+                    inStack[u] = 1;
+                } else {
+                    stack.pop();
+                    inStack[u] = 0;
+                }
+
+                for (int v : graph.get(u)) {
+                    if (visited[v] == 0) stack.push(v);
+                    if (inStack[v] == 1) return 1;
+                }
+            }
+        }
+
+        return 0;
     }
 }
